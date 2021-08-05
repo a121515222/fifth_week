1d395e9869d4194f3bd09de38ed7eebf225ba70f
a121515222 <85506228+a121515222@users.noreply.github.com>
2021-08-06 02:00:46 +0800

    'update'

--- app/assets/js/fifth-week.js
+++ app/assets/js/fifth-week.js
@@ -1,18 +1,20 @@
+
 $(document).ready(function () {
+     //彈出CKEditor
 $('.switch').click(function (e) { 
     e.preventDefault();
     $('.slide_page').toggleClass('active');
     
 });
+
 });
 
 
 /*DOM*/
 const stopa = document.querySelector('.for_preventDefault');
-const stopa2 = document.querySelector('.for_preventDefault');
+
 /*/*取消 main_admin a標籤visibility and edit icon 的連結功能*/
 function stopalink(e){
     e.preventDefault()
 }
 stopa.addEventListener('click',stopalink,false)
-stopa2.addEventListener('click',stopalink,false)
